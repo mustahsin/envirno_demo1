d4ee8912584616aab301911c6457dd01b648737e
Siam <moula@revesoft.com>
2018-11-23 19:22:47 +0600

    test

---
+++ src/components/tables/Tables.vue
@@ -0,0 +1,177 @@
+<template>
+  <div>
+    <div class="">
+      <div class="col-md-8">
+        <card header-text="Current Water Data Table">
+          <div class="table-responsive">
+            <table class="table table-sm color-icon-label-table table-hover table-bordered" >
+              <thead>
+              <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Parameters</th>
+                  <th scope="col">Current value</th>
+                  <th scope="col">24 hour avg</th>
+                  <th scope="col">Reference</th>
+              </tr>
+              </thead>
+              <tbody>
+              <tr v-for="item in 4" :key="item">
+                <td>{{item}}</td>
+                <td>{{Object.keys(currentData)[item-1]}}</td>
+                <td>{{currentData[Object.keys(currentData)[item-1]]}}</td>
+                <td>{{avgData[item-1]}}</td>
+                <td>{{referenceData[Object.keys(referenceData)[item-1]]}}</td>
+              </tr>
+              </tbody>
+            </table>
+          </div>
+        </card>
+      </div>
+    </div>
+
+    <!-- <div class="row">
+      <div class="col-md-12">
+        <card header-text="Search & Pagination">
+          <data-table :apiUrl="apiUrl"
+              :tableFields="tableFields"
+              :itemsPerPage="itemsPerPage"
+              :sortFunctions="sortFunctions"
+              :apiMode="apiMode"
+              :paginationPath="paginationPath"
+          ></data-table>
+        </card>
+      </div>
+    </div> -->
+
+  </div>
+</template>
+
+
+<script>    
+    import Vue from 'vue'
+
+    import DataTable from './vuetable-2/DataTable.vue'
+    import BadgeColumn from './BadgeColumn.vue'
+    
+    Vue.component('badge-column', BadgeColumn);
+
+    import FieldsDef from './vuetable-2/data/fields-definition';
+    import ItemsPerPageDef from './vuetable-2/data/items-per-page-definition';
+
+    export default {
+        name: 'Table',
+        components: {
+          DataTable
+        },
+        
+        data () {
+            return {
+              apiUrl: 'https://vuetable.ratiw.net/api/users',
+              apiMode: true,
+              tableFields: FieldsDef.tableFields,
+              itemsPerPage: ItemsPerPageDef.itemsPerPage,
+              sortFunctions: FieldsDef.sortFunctions,
+              paginationPath: '',
+              allData:[],
+              avgData:[],
+              currentData:{
+                Temperature:20,
+                  PH:3.0,
+                  Humidity:78,
+                  Flow_speed:7
+              },
+              referenceData:{
+                Temperature:28,
+                  PH:7.0,
+                  Humidity:68,
+                  Flow_speed:6
+              }
+          }
+          
+        },
+        methods:
+        {
+          getTotalParameter()
+          {
+            this.totalParameters = 4
+          },
+          addrealData()
+          {
+            for (var i=1;i<10;i++)
+            {
+              var temp = (i+1)*1;
+              var ph = 3.5 + (i*0.1);
+              var hum = 50 + (i*0.5);
+              var flow = 4.5 + (i*0.4);
+
+              var dict = {};
+              dict['Temperature'] = temp;
+              dict['PH'] = ph;
+              dict['Humidity'] = hum;
+              dict['Flow_speed'] = flow;
+
+              this.allData.push(dict);
+            }
+          },
+          getAverageFor(index)
+          {
+            var sum = 0;
+              var res=0;
+              for(var i=0;i<this.allData.length;i++)
+              {
+                  var entity = this.allData[i];
+                  var keyName = Object.keys(entity)[index];
+                  var paramValue = this.allData[i][keyName];
+                  
+                  sum = sum+paramValue;
+              }
+              res = sum/this.allData.length;
+              res = res.toFixed(2);
+              //alert("res : " + res);
+              return res;
+          },
+          assignAvg ()
+          {
+            var dict = {};
+            var avg_temp,avg_ph,avg_hum,avg_flow;
+            avg_temp = this.getAverageFor(0);
+            avg_ph = this.getAverageFor(1);
+            avg_hum = this.getAverageFor(2);
+            avg_flow = this.getAverageFor(3);
+
+            this.avgData.push(avg_temp);
+            this.avgData.push(avg_ph);
+            this.avgData.push(avg_hum);
+            this.avgData.push(avg_flow);
+          }
+        },
+        created ()
+        {
+          this.addrealData();
+          this.assignAvg();
+          //alert("temperature average : " + tempAverage);
+
+        },
+    }
+</script>
+
+<style lang="scss">
+  .table thead tr{
+    color: #007bff;
+  }
+  .error-icon{
+    color: #e34a4a;
+  }
+  .success-icon {
+    display: inline-block;
+    color: #4ae387;
+  }
+  .table-sm th, .table-sm td{
+    padding: .7rem;
+  }
+  .color-icon-label-table {
+    td:first-child {
+      width: 1rem;
+    }
+  }
+</style>
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="row">
+        <Table/>
+        <Revenue/>
+    </div>
+
+</template>
+
+<script>
+import Info from './dashboard/InfoBox.vue';
+import InfoChart from './dashboard/InfoBoxChart.vue';
+import InfoChartTwo from './dashboard/InfoBoxChart2.vue';
+import RealTime from './dashboard/RealTime.vue';
+import Traffic from './dashboard/Traffic.vue';
+import EarningStats from './dashboard/EarningStats.vue';
+import Download from './dashboard/Download.vue';
+import Revenue from './dashboard/Revenue.vue';
+import Table from '../../src/components/tables/Tables.vue';
+
+
+export default{
+    name: 'dashboard',
+    components: {
+        Info,
+        InfoChart,
+        InfoChartTwo,
+        RealTime,
+        Traffic,
+        EarningStats,
+        Download,
+        Revenue,
+        Table
+    }
+}
+
+
+</script>
+
+<style>
+
+</style>
